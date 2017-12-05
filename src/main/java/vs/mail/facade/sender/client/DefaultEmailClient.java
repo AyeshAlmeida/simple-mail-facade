3590ed45f1cc50967df0931c4d43273a6f7b95e4
AyeshAlmeida <ayesh9206@gmail.com>
2017-12-06 00:40:58 +0530

    Fix: Minor Refactoring.

--- src/main/java/vs/mail/facade/sender/client/DefaultEmailClient.java
+++ src/main/java/vs/mail/facade/sender/client/DefaultEmailClient.java
@@ -6,22 +6,15 @@ import vs.mail.facade.api.config.Configuration;
 import vs.mail.facade.api.config.SecType;
 import vs.mail.facade.api.email.Email;
 import vs.mail.facade.api.response.EmailResponse;
-import vs.mail.facade.exception.SecurityException;
 import vs.mail.facade.sender.executor.DefaultExecutor;
 import vs.mail.facade.sender.executor.EmailExecutor;
 import vs.mail.facade.sender.helper.EmailClientHelper;
 
-import javax.mail.Session;
 import java.util.Optional;
-import java.util.Properties;
-
-import static vs.mail.facade.config.EmailPropertyBuilder.getPropertiesForSslSecurity;
-import static vs.mail.facade.config.EmailPropertyBuilder.getPropertiesForTlsSecurity;
-import static vs.mail.facade.util.EmailClientHelper.getAuthenticator;
 
 public final class DefaultEmailClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultEmailClient.class);
-    private static final EmailExecutor executor = DefaultExecutor.getDefaultExecutor();
+    private static final EmailExecutor emailExecutor = DefaultExecutor.getDefaultExecutor();
     private final Configuration configuration;
 
     public DefaultEmailClient(Configuration configuration) {
@@ -32,15 +25,15 @@ public final class DefaultEmailClient {
         LOGGER.info("Sending Email [{}] with Configurations [{}]", email, configuration);
         if (configuration.getSecType().equals(SecType.NONE)) {
             return Optional.of(
-                    EmailClientHelper.sendWithoutSecurity(email, configuration, executor));
+                    EmailClientHelper.sendWithoutSecurity(email, configuration, emailExecutor));
         }
         if (configuration.getSecType().equals(SecType.SSL)) {
             return Optional.of(
-                    EmailClientHelper.sendWithSsl(email, configuration, executor));
+                    EmailClientHelper.sendWithSsl(email, configuration, emailExecutor));
         }
         if (configuration.getSecType().equals(SecType.TLS)) {
             return Optional.of(
-                    EmailClientHelper.sendWithTls(email, configuration, executor));
+                    EmailClientHelper.sendWithTls(email, configuration, emailExecutor));
         }
         return Optional.empty();
     }
