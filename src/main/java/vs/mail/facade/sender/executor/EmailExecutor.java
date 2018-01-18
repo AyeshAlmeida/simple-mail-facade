fafd9e6a776dea09684ef87548365c920b17be5e
AyeshAlmeida <ayesh9206@gmail.com>
2018-01-18 11:58:45 +0530

    Update: Minor Refactoring.

--- src/main/java/vs/mail/facade/sender/executor/EmailExecutor.java
+++ src/main/java/vs/mail/facade/sender/executor/EmailExecutor.java
@@ -6,5 +6,5 @@ import vs.mail.facade.api.response.EmailResponse;
 import javax.mail.Session;
 
 public interface EmailExecutor {
-    public EmailResponse fireEmail(final Email email, final Session session);
+    EmailResponse fireEmail(final Email email, final Session session);
 }
